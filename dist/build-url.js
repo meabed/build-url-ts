35ddf394857cd22d4e5a7cfb0b27ca471aedaea6
Steve Rydz <steverydz@gmail.com>
2018-06-27 16:56:54 +0100

    Bump version

--- dist/build-url.js
+++ dist/build-url.js
@@ -1,6 +1,6 @@
 /**
  * build-url - A small library that builds a URL given its components
- * @version v1.1.1
+ * @version v1.1.2
  * @link https://github.com/steverydz/build-url#readme
  * @license MIT
  */
