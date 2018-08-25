8b05bd817cae331c16c3c1dd4eeff160343659c8
Steve Rydz <steverydz@gmail.com>
2018-08-25 23:33:08 +0100

    Update packages

--- dist/build-url.js
+++ dist/build-url.js
@@ -1,6 +1,6 @@
 /**
  * build-url - A small library that builds a URL given its components
- * @version v1.1.2
+ * @version v1.2.0
  * @link https://github.com/steverydz/build-url#readme
  * @license MIT
  */
