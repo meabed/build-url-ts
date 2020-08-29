15c8526b69759e879b21d1f1c12c2671c53d2807
Steve Rydz <steverydz@gmail.com>
2020-08-29 10:44:46 +0100

    Strip out header from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,8 @@
 const gulp = require("gulp");
-const header = require("gulp-header");
 const pump = require("pump");
-const pkg = require("./package.json");
-const banner = [
-  "/**",
-  " * <%= pkg.name %> - <%= pkg.description %>",
-  " * @version v<%= pkg.version %>",
-  " * @link <%= pkg.homepage %>",
-  " * @license <%= pkg.license %>",
-  " */\n",
-].join("\n");
 
 function copy(cb) {
-  pump(
-    [
-      gulp.src("./src/build-url.js"),
-      header(banner, { pkg: pkg }),
-      gulp.dest("./dist"),
-    ],
-    cb
-  );
+  pump([gulp.src("./src/build-url.js"), gulp.dest("./dist")], cb);
 }
 
 exports.default = gulp.series(copy);
