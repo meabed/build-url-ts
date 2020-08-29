dbcb46d95beb5ac1b8a9edf6a5df5412c8a6b92c
Steve Rydz <steverydz@gmail.com>
2020-08-29 10:55:30 +0100

    Remove gulp and replace with webpack

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require("gulp");
-const pump = require("pump");
-
-function copy(cb) {
-  pump([gulp.src("./src/build-url.js"), gulp.dest("./dist")], cb);
-}
-
-exports.default = gulp.series(copy);
