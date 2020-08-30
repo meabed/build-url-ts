2916408c1a54a083e427896f3a5428df9da55289
Mohamed Meabed <mo.meabed@gmail.com>
2020-08-31 01:23:06 +0400

    chore: version pinning and build fixes

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript';
-import pkg from './package.json'
 
 export default {
   input: 'src/build-url.ts',
@@ -9,9 +8,5 @@ export default {
     dir: 'dist',
     format: 'cjs'
   },
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
   plugins: [typescript()]
 };
