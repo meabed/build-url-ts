e518cad563baee6031d425611e9165dc39b6570d
Steve Rydz <steverydz@gmail.com>
2020-08-30 18:18:16 +0100

    Fix main file

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: {
-    bulidUrl: "./src/build-url.js",
+    "build-url": "./src/build-url.js",
   },
   output: {
     filename: "[name].js",
