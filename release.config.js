068259e03c1a396fefce78dbe271d251781d2092
Mohamed Meabed <mo.meabed@gmail.com>
2022-12-27 06:49:18 -0500

    chore: update ci

--- release.config.js
+++ release.config.js
@@ -31,11 +31,18 @@ module.exports = {
       {
         preset: 'conventionalcommits',
         releaseRules: [
-          {
-            type: 'build',
-            scope: 'deps',
-            release: 'patch'
-          }
+          { type: 'breaking', release: 'major' },
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'revert', release: 'patch' },
+          { type: 'docs', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'test', release: 'patch' },
+          { type: 'chore', release: 'patch' },
+          { type: 'ci', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'build', release: 'patch' },
         ]
       }
     ],
